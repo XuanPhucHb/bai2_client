4ad648f878d38d08d3e4e127f4581d6d18408b93
XuanPhucHb <xuanphuchb@gmail.com>
2021-01-15 20:54:04 +0700

    first commit

---
+++ Web_Client/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_Client.Models
+{
+    public class Member
+    {
+        [Display(Name ="Id")]
+        public int Id { get; set; }
+        [Display(Name = "Tiêu đề")]
+        public string Title { get; set; }
+        [Display(Name = "Link ảnh")]
+        public string Thumb { get; set; }
+        [Display(Name = "Nội dung")]
+        public string Description { get; set; }
+    }
+}
